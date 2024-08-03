60d357b7a0b83a7046cb299f92d739c40bbc3cd4
Paul Fuqua <pf@acm.org>
2024-08-03 17:14:24 -0500

    Initial commit.  - check-ci-status.py reports on our nightly and weekly builds.  - node-status.py and mlir-nodes.py report the status of nodes in our CI systems.

---
+++ check-ci-status.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import datetime
+import contextlib
+import jenkins
+import dominate
+import dominate.tags as dt
+
+def build_status(server, buildName):
+    job_info = server.get_job_info(buildName)
+    last  = job_info['lastBuild']['number']
+    compl = job_info['lastCompletedBuild']['number']
+    succ  = job_info['lastSuccessfulBuild']['number']
+    if last > compl: # Not 100% sure if this is how I want to display it.
+        compl_info = server.get_build_info(buildName, last)
+    else:
+        compl_info = server.get_build_info(buildName, compl)
+    if compl != succ:
+        return compl_info, server.get_build_info(buildName, succ)
+    else:
+        return compl_info, None
+
+def failed_stages(server, buildName, howMany=50):
+    job_info = server.get_job_info(buildName)
+    last_completed = job_info['lastCompletedBuild']['number']
+    for job in range(last_completed, last_completed-howMany, -1):
+        stages = server.get_build_stages(buildName, job)
+        failing = [stage for stage in stages['stages'] if stage['status'] == 'FAILED' and stage['name'] != 'Environment']
+        if failing:
+            print(str(job)+" : ", ', '.join([stage['name'] for stage in failing][0:1]))
+
+
+servers = [{'host' : 'http://ml-ci.amd.com:21096',
+            'passwordfile' : '.jenkins.ml-ci',
+            'builds' : [('MLIR/check-mlir-nightly-all', 'non-xdlops nightly'),
+                        ('MLIR/mlir-weekly', 'non-xdlops weekly')]},
+           {'host' : 'http://ml-ci-internal.amd.com:8080',
+            'passwordfile' : '.jenkins.ml-ci-internal',
+            'builds' : [('mlir/mlir-nightly-all', 'xdlops nightly'),
+                        ('mlir/mlir-weekly', 'xdlops weekly')]}
+]
+
+def init_servers(servers):
+    for server in servers:
+        with open(os.path.expanduser(f"~/{server['passwordfile']}")) as f:
+            user,password = f.readlines()[0].rstrip().split(':')
+            server['connection'] = jenkins.Jenkins(server['host'],
+                                                   username=user,
+                                                   password=password)
+    return servers
+
+def run(htmlp=False):
+    if htmlp:
+        doc = dominate.document(title=None)
+    else:
+        doc = contextlib.nullcontext()
+    with doc:
+        for server in init_servers(servers):
+            try:
+                for buildName,prettyName in server['builds']:
+                    compl_info, succ_info = build_status(server['connection'], buildName)
+                    # I don't know why it insists on short name, but my use requires the FQDN.
+                    url = compl_info['url']
+                    started = datetime.datetime.fromtimestamp(round(compl_info['timestamp']/1000.0))
+                    took = datetime.timedelta(milliseconds=round(compl_info['duration']/1000.0)*1000)
+                    result = compl_info['result']
+                    if htmlp:
+                        style=None
+                        if result == 'SUCCESS':
+                            style = "color:green;"
+                        elif result == 'FAILURE':
+                            style = "color:red;"
+                        dt.p(prettyName, dt.a(compl_info['id'], href=url), " : ",
+                             dt.span(result, style=style),
+                             f"  (started {started}, took {took})")
+                    else:
+                        print(url+" : ", result)
+                        print(f"    started {started}, took {took}")
+                    if succ_info:
+                        succ_started = datetime.date.fromtimestamp(round(succ_info['timestamp']/1000.0))
+                        if htmlp:
+                            dt.p(f"(last successful:  {succ_info['number']} on {succ_started})",
+                                 style="text-indent: 40px")
+                        else:
+                            print(f"    (last successful:  {succ_info['number']} on {succ_started})")
+            except Exception as err:
+                print(f"Error: {err}")
+    if htmlp:
+        print(doc)
+
+
+if __name__ == '__main__':
+    try:
+        run(len(sys.argv) > 1)
+    except Exception as err:
+        print(f"Error: {err}")
---
+++ mlir-nodes.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import time
+import math
+import jenkins
+import tabulate
+import re
+
+servers = [{'host' : 'http://ml-ci.amd.com:21096',
+            'passwordfile' : '.jenkins.ml-ci',
+            'builds' : ['MLIR/check-mlir-nightly-all', 'MLIR/mlir-weekly']},
+           {'host' : 'http://ml-ci-internal.amd.com:8080',
+            'passwordfile' : '.jenkins.ml-ci-internal',
+            'builds' : ['mlir/mlir-nightly-all', 'mlir/mlir-weekly']}
+]
+
+def init_servers(servers):
+    for server in servers:
+        with open(os.path.expanduser(f"~/{server['passwordfile']}")) as f:
+            user,password = f.readlines()[0].rstrip().split(':')
+            server['connection'] = jenkins.Jenkins(server['host'],
+                                                   username=user,
+                                                   password=password)
+    return servers
+
+def xdlop_nodes(server):
+    nodes = []
+    for n in server.get_nodes(depth=1):
+        #  not n['offline'] and
+        if n['name'] != 'Built-In Node':
+            name = n['name']
+            info = server.get_node_info(name, depth=1)
+            labels = [x['name'] for x in info['assignedLabels'] if x['name'] != name]
+            #  and ('gfx908' in labels or 'gfx90a' in labels):
+            if 'mlir' in labels:
+                note = ''
+                if info['offline']:
+                    note = ' (offline)'
+                elif info['idle']:
+                    note = ' (idle)'
+                nodes.append([name+note, ','.join(labels)])
+#                print(f"{name}{note}: \t{labels}")
+    return nodes
+
+def job_queue(server, show_why=False):
+    queue = []
+    for j in reversed(server.get_queue_info()):
+        detail      = server.get_queue_item(j['id'], depth=1)
+        name        = detail['task']['name']
+        queued_time = j['inQueueSince'] / 1000.0
+        now_time    = time.mktime(time.localtime())
+        time_diff   = now_time - queued_time
+        if time_diff > 86400:
+            start_time = f"{math.floor(time_diff / 86400.0)} days"
+            if time_diff < 2*86400:
+                start_time = start_time[:-1]
+        else:
+            start_time = time.strftime('%l:%M%p', time.localtime(queued_time))
+        if show_why:
+            why = j['why']
+            if len(why) > 80:
+                why = why[0:79] + '....'
+            queue.append([j['url'][11:-1], start_time, name, why])
+        else:
+            queue.append([j['url'][11:-1], start_time, name])
+#        print(f"{j['url'][11:-1]} ({start_time}): {j['why']}")
+    return queue
+
+# nodes which have all the given labels
+def nodes_with_labels(server, labels):
+    nodes = []
+    for n in server.get_nodes(depth=1):
+        #  not n['offline'] and
+        if n['name'] != 'Built-In Node':
+            name = n['name']
+            info = server.get_node_info(name, depth=2)
+            node_labels = [x['name'] for x in info['assignedLabels'] if x['name'] != name]
+            second = None
+            if 'all' in labels or all([l in node_labels for l in labels]):
+                note = ''
+                if info['offline']:
+                    note = ' (offline)'
+                    if info['offlineCauseReason'] == 'Offline because computer was idle; it will be relaunched when needed.':
+                        note = ' (idle*)'
+                    else:
+                        second = info['offlineCauseReason']
+                elif info['idle']:
+                    note = ' (idle)'
+                else:
+                    ex = info['executors'][0]
+                    ce = ex['currentExecutable']
+                    if ce:
+                        second = ce['displayName']
+                arch = [l for l in node_labels if re.match('^gfx', l)]
+                if arch:
+                    arch = arch[0]
+                    node_labels.remove(arch)
+                    node_labels.insert(0, arch)
+                nodes.append([name+note, ' '.join(node_labels)])
+                if second:
+                    nodes.append(['  ', second])
+#                print(f"{name}{note}: \t{labels}")
+    return nodes
+
+def run(labels, show_queue=True, show_why=False):
+    for server in init_servers(servers):
+        conn = server['connection']
+        print(f"On {server['host']}:")
+        print(tabulate.tabulate(nodes_with_labels(conn, labels), tablefmt='plain'))
+        if show_queue:
+            print('')
+            print(tabulate.tabulate(job_queue(conn, show_why), tablefmt='plain'))
+        print('')
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--queue', action='store_true',
+                    help="show the job queue")
+parser.add_argument('--why', action='store_true',
+                    help="also show the reason the job is queued")
+parser.add_argument('--labels', default='mlir', type=str,
+                    help='comma-separated list of labels, or "all" to match any label')
+args = parser.parse_args()
+
+if __name__ == '__main__':
+    try:
+        run(labels=args.labels.split(','), show_queue=args.queue, show_why=args.why)
+    except Exception as err:
+        print(f"Error: {err}")
